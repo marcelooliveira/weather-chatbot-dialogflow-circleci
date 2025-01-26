a66c2b02359a1cbb38c629aa4e22548d560f3141
marcelooliveira <mclricardo@gmail.com>
2025-01-26 16:58:28 -0300

    Intent GetCountryName

--- function_app.py
+++ function_app.py
@@ -31,6 +31,38 @@ def parse_lat_lon(user_input):
         return lat, lon
     except:
         return None, None
+    
+def get_weather_for_coords_http_response(location, lat, lon):
+    # Call OpenWeatherMap API
+    weather_url = "https://api.openweathermap.org/data/2.5/weather"
+    response = requests.get(weather_url, params={"units": "metric", "lat": lat, "lon": lon, "appid": OPENWEATHERMAP_API_KEY})
+
+    if response.status_code == 200:
+        weather_data = response.json()
+        weather_main = weather_data["weather"][0]["description"]
+        temperature = weather_data["main"]["temp"]
+        weather_message = f"The weather for {location} is {weather_main} with a temperature of {temperature}ºC."
+        return func.HttpResponse(
+            json.dumps({
+                "fulfillmentText": weather_message
+            }),
+            status_code=200
+        )                
+    else:
+        return func.HttpResponse(
+            "Failed to fetch weather data. Please try again later.",
+            status_code=500
+        )
+
+    
+def get_coords(city, state, country):
+    # Call OpenWeatherMap API
+    geo_url = f"http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&limit=1&appid={OPENWEATHERMAP_API_KEY}"
+    print(geo_url)
+    response = requests.get(geo_url)
+
+    geo_data = response.json()
+    return geo_data
 
 # Azure Function App definition
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
@@ -47,14 +79,13 @@ def webhook(req: func.HttpRequest) -> func.HttpResponse:
         # Parse the incoming request JSON
         req_body = req.get_json()
         
-        intent = req_body['queryResult']['intent']['displayName']
-        query_text = req_body['queryResult']['queryText']
-
+        query_result = req_body['queryResult']
+        intent = query_result['intent']['displayName']
         print(f"intent: {intent}")
-        print(f"query_text: {json.dumps(req_body['queryResult'])}")
 
-        # Handle "GetWeatherByCoordinates" intent
         if intent == "GetWeatherByCoordinates":
+            query_text = query_result['queryText']
+            print(f"query_text: {json.dumps(req_body['queryResult'])}")
             lat, lon = parse_lat_lon(query_text)
             if lat is None or lon is None:
                 return func.HttpResponse(
@@ -62,26 +93,43 @@ def webhook(req: func.HttpRequest) -> func.HttpResponse:
                     status_code=200
                 )
 
-            # Call OpenWeatherMap API
-            weather_url = "https://api.openweathermap.org/data/2.5/weather"
-            response = requests.get(weather_url, params={"units": "metric", "lat": lat, "lon": lon, "appid": OPENWEATHERMAP_API_KEY})
+            location_http_response = None
 
-            if response.status_code == 200:
-                weather_data = response.json()
-                weather_main = weather_data["weather"][0]["description"]
-                temperature = weather_data["main"]["temp"]
-                weather_message = f"The weather for the given location is {weather_main} with a temperature of {temperature}ºC."
-                return func.HttpResponse(
-                    json.dumps({
-                        "fulfillmentText": weather_message
-                    }),
-                    status_code=200
-                )                
-            else:
-                return func.HttpResponse(
-                    "Failed to fetch weather data. Please try again later.",
-                    status_code=500
-                )
+            location = f"lat {lat:.2f} lon {lon:.2f}"
+
+            weather_response = get_weather_for_coords_http_response(location, lat, lon)
+
+            return weather_response
+
+        if intent == "GetCountryName":
+            # Extracting values from the dictionary
+            geo_city = None
+            geo_state = None
+            geo_country = None
+
+            # Loop through outputContexts to find the context containing all three parameters
+            for context in query_result["outputContexts"]:
+                params = context.get("parameters", {})
+                if "geo-city" in params and "geo-state" in params and "geo-country" in params:
+                    geo_city = params["geo-city.original"]
+                    geo_state = params["geo-state.original"]
+                    geo_country = params["geo-country.original"]
+                    break
+
+            # Print the extracted variables
+            print("Geo-City:", geo_city)
+            print("Geo-State:", geo_state)
+            print("Geo-Country:", geo_country)
+
+            coords = get_coords(geo_city, geo_state, geo_country)
+            lat = coords[0]["lat"]
+            lon = coords[0]["lon"]
+
+            location = f"{geo_city} / {geo_state} / {geo_country}"
+
+            weather_response = get_weather_for_coords_http_response(location, lat, lon)
+
+            return weather_response
 
         # Default response for unhandled intents
         return func.HttpResponse(
